371d401941a90ba8bc81e21a28c7ff0d422fc36a
David Nagarpowers <dnagarpowers@gmail.com>
2023-06-22 15:04:18 -0400

    fix

--- src/components/Byline.tsx
+++ src/components/Byline.tsx
@@ -11,6 +11,7 @@ const Byline = () => {
         <a href="#contact" className="btn btn--pink">
           Get in touch
         </a>
+        <a href="https://www.buymeacoffee.com/dnagarpowers" className="btn"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=dnagarpowers&button_colour=5F7FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a>
       </div>
     </div>
   );
