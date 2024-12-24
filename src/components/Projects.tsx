43f6f614dc2023b5494399483b84418d3f07b518
danyeric123 <dnagarpowers@gmail.com>
2024-12-24 13:34:16 -0500

    forewarning

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,6 +6,9 @@ const Projects = () => {
     <section className="work" id="projects">
       <div className="row">
         <h2>Projects</h2>
+        <p className="preceding-text">
+          These are some of my projects. Much of my work can be found on my <a href="https://github.com/danyeric123" target="_blank" rel="noopener noreferrer">GitHub</a> or cannot be shared because it was done at work.
+        </p>
         {projects.map((project) => (
           <>
             <Project project={project} />
